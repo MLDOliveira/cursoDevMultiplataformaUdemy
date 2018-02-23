c2bec6dbcc27bc84bb0f0149a2cd0ff1df409e6f
Mateus Oliveira <mateus.oliveira@itambe.com.br>
2018-02-23 16:42:23 -0300

    Finalizando APP2 e iniciando APP3

---
+++ app3/index.android.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+
+class MeuComponente extends Component{
+  render(){
+    return (
+      <View>
+        <Text>{this.props.teste}</Text>
+      </View>
+    );
+  }
+}
+
+class app3 extends Component{
+
+  constructor(props) {
+    super(props);
+
+    this.state = { texto : 'Texto teste 2' };
+  }
+
+  alteraTexto() {
+    this.setState({ texto : 'Outra coisa' });
+  }
+
+  render() {
+    return (
+      <View>
+      <MeuComponente teste={this.state.texto}></MeuComponente>
+      <Button 
+        onPress={() => { this.alteraTexto()}}
+        title='Botão'
+      />
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('app3', () => app3);
