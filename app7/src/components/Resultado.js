636efbaafcac70006d9c34445be6c291fa15510c
MLDOliveira <mateusluisoliveira@gmail.com>
2018-03-05 20:57:00 -0300

    Continuação do APP7
    Criando componentes funcionais de Topo, Resultado e Painel.

---
+++ app7/src/components/Resultado.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Resultado = props => (
+    <View>
+        <Text> Resultado </Text>
+    </View>
+);
+
+export { Resultado };
