1dff53b40abf62e6c3efd8f25aa3c42f5a2a683e
MLDOliveira <mateusluisoliveira@gmail.com>
2018-03-13 20:56:20 -0300

    APP com Firebase
    Aprendendo manipulação básica de dados com firebase

---
+++ firebaseTeste/index.android.js
@@ -0,0 +1,78 @@
+import firebase from 'firebase';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  View,
+  Text, 
+  Button
+} from 'react-native';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      pontuacao: 0
+    }
+  }
+
+
+  componentWillMount() {
+    var config = {
+      apiKey: "AIzaSyAzax8Mq-GEPXqmLifUDUK_Kn9oBLaV7jc",
+      authDomain: "configuracaofirebasereac-df197.firebaseapp.com",
+      databaseURL: "https://configuracaofirebasereac-df197.firebaseio.com",
+      projectId: "configuracaofirebasereac-df197",
+      storageBucket: "configuracaofirebasereac-df197.appspot.com",
+      messagingSenderId: "1018427000522"
+    };
+    firebase.initializeApp(config);
+  }
+
+  salvarDados() {
+    var funcionarios = firebase.database().ref("funcionarios");
+    //database.ref("pontuacao").set("120");
+    //database.ref("pontuacao").remove();
+
+    //funcionarios.push().child("nome").set("Mateus");
+    funcionarios.push().set(
+      {
+        nome: "Mateus",
+        altura: "1,75",
+        peso: "8KG"
+      }
+    )
+  }
+
+  listarDados() {
+    var pontuacao = firebase.database().ref("pontuacao");
+    pontuacao.on('value', (snapshot) => {
+      var pontos = snapshot.val();
+      this.setState( { pontuacao: pontos });
+    });
+
+  }
+  render() {
+    let {pontuacao} = this.state;
+
+    return (
+      <View >
+        <Button 
+          onPress={ ()=>{ this.salvarDados(); }}
+          title="Salvar dados"
+          color="#841584"
+          accessibilityLaber="Salvar dados"
+        />
+        <Button 
+          onPress={ ()=>{ this.listarDados(); }}
+          title="Listar dados"
+          color="#841584"
+          accessibilityLaber="Listar dados"
+        />
+        <Text>{pontuacao}</Text>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('firebaseTeste', () => App);
